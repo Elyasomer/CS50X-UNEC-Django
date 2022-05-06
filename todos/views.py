f044c9fd124e3693ed399d1d0b89ab9ee284d777
Elyas <elyas.ghausi@gmail.com>
2022-05-06 20:32:25 +0400

    login page added

--- todos/views.py
+++ todos/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.contrib.auth import login, authenticate
 
 # Create your views here.
+def loginPage(request):
+    
+    # print(request.GET.get('q'))
+
+    if request.method == 'POST':
+        req = request.POST
+        username = req.get('username')
+        password = req.get('pass')
+        user = authenticate(request, username=username, password=password)
+        if user is not False:
+            login(request, user)
+    return render(request, 'login.html')
